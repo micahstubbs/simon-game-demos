2f0c24ced3e2d14f75c39a756afa5dddc1a75c11
Micah Stubbs <micah.stubbs@gmail.com>
2017-10-25 15:06:37 -0700

    get row working for grid layout

--- svg-d3/simon.js
+++ svg-d3/simon.js
@@ -30,16 +30,31 @@ svg
 // draw the simon board
 // composed of the 4 small squares
 // which we call buttons
-const offset = margin.left + baseSide + gutter;
+function squareGridLayout(data, cellSide, gutter) {
+  gridSide = Math.ceil(Math.sqrt(data.length));
+  console.log('gridSide', gridSide);
+  let currentRow = 0;
+  let rowOffset = 0;
+  data.forEach((d, i) => {
+    d.x = gutter + (gutter + cellSide) * (i - currentRow);
+    d.y = gutter + (gutter + cellSide) * i;
+    if (i % gridSide === 0) currentRow += 1;
+    console.log(`i ${i} currentRow ${currentRow} mod ${i % gridSide}`)
+  })
+  return data;
+}
+
+const xOffset = margin.left + baseSide;
+
 const buttonSide = 100;
 const buttonsG = svg
   .append('g')
   .selectAll('rect')
-  .data(buttons)
+  .data(squareGridLayout(buttons, buttonSide, gutter))
   .enter()
   .append('rect')
-  .attr('x', (d, i) => offset + (buttonSide + gutter) * i)
-  .attr('y', margin.top)
+  .attr('x', (d, i) => xOffset + d.x)
+  .attr('y', d => d.y)
   .attr('width', buttonSide)
   .attr('height', buttonSide)
   .style('fill', d => d.color);
