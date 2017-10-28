b82ca458ced5bd92f11f9d3caaf2b07101c80db5
Micah Stubbs <micah.stubbs@gmail.com>
2017-10-27 21:15:45 -0700

    white background

--- webgl/simon.js
+++ webgl/simon.js
@@ -164,7 +164,7 @@ function webGLStart() {
   initShaders();
   initBuffers();
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
   gl.enable(gl.DEPTH_TEST);
 
   drawScene();
