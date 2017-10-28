11177804b4c38e546dda8f86ce6103f72b95e9c2
Micah Stubbs <micah.stubbs@gmail.com>
2017-10-27 21:15:20 -0700

    draw only a square

--- webgl/simon.js
+++ webgl/simon.js
@@ -99,20 +99,6 @@ var squareVertexPositionBuffer;
 var squareVertexColorBuffer;
 
 function initBuffers() {
-  triangleVertexPositionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-  var vertices = [0.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, -1.0, 0.0];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-  triangleVertexPositionBuffer.itemSize = 3;
-  triangleVertexPositionBuffer.numItems = 3;
-
-  triangleVertexColorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer);
-  var colors = [1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-  triangleVertexColorBuffer.itemSize = 4;
-  triangleVertexColorBuffer.numItems = 3;
-
   squareVertexPositionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
   vertices = [1.0, 1.0, 0.0, -1.0, 1.0, 0.0, 1.0, -1.0, 0.0, -1.0, -1.0, 0.0];
@@ -146,28 +132,6 @@ function drawScene() {
   mat4.identity(mvMatrix);
 
   mat4.translate(mvMatrix, [-1.5, 0.0, -7.0]);
-  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-  gl.vertexAttribPointer(
-    shaderProgram.vertexPositionAttribute,
-    triangleVertexPositionBuffer.itemSize,
-    gl.FLOAT,
-    false,
-    0,
-    0
-  );
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer);
-  gl.vertexAttribPointer(
-    shaderProgram.vertexColorAttribute,
-    triangleVertexColorBuffer.itemSize,
-    gl.FLOAT,
-    false,
-    0,
-    0
-  );
-
-  setMatrixUniforms();
-  gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
 
   mat4.translate(mvMatrix, [3.0, 0.0, 0.0]);
   gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
